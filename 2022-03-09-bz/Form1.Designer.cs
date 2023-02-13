2333810302c47d04929a2e995e15b90448386730
zollerkmd <bukizoller@gmail.com>
2023-02-13 06:51:45 +0100

    Initial commit

---
+++ 2022-03-09-bz/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿
+namespace _2022_03_09_bz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbNev = new System.Windows.Forms.TextBox();
+            this.tbDarab = new System.Windows.Forms.TextBox();
+            this.tbAr = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnGeneral = new System.Windows.Forms.Button();
+            this.cbTorekeny = new System.Windows.Forms.CheckBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblOsszertek = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbHozzafuz = new System.Windows.Forms.RadioButton();
+            this.rbUresLista = new System.Windows.Forms.RadioButton();
+            this.btnTorol = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Név";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Darab";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ár";
+            // 
+            // tbNev
+            // 
+            this.tbNev.Location = new System.Drawing.Point(68, 39);
+            this.tbNev.Name = "tbNev";
+            this.tbNev.Size = new System.Drawing.Size(143, 20);
+            this.tbNev.TabIndex = 3;
+            // 
+            // tbDarab
+            // 
+            this.tbDarab.Location = new System.Drawing.Point(68, 65);
+            this.tbDarab.Name = "tbDarab";
+            this.tbDarab.Size = new System.Drawing.Size(85, 20);
+            this.tbDarab.TabIndex = 4;
+            // 
+            // tbAr
+            // 
+            this.tbAr.Location = new System.Drawing.Point(68, 91);
+            this.tbAr.Name = "tbAr";
+            this.tbAr.Size = new System.Drawing.Size(114, 20);
+            this.tbAr.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(188, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(23, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = ">";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(188, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(23, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnGeneral
+            // 
+            this.btnGeneral.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnGeneral.Location = new System.Drawing.Point(159, 64);
+            this.btnGeneral.Name = "btnGeneral";
+            this.btnGeneral.Size = new System.Drawing.Size(23, 23);
+            this.btnGeneral.TabIndex = 8;
+            this.btnGeneral.Text = "G";
+            this.btnGeneral.UseVisualStyleBackColor = true;
+            this.btnGeneral.Click += new System.EventHandler(this.btnGeneral_Click);
+            // 
+            // cbTorekeny
+            // 
+            this.cbTorekeny.AutoSize = true;
+            this.cbTorekeny.Location = new System.Drawing.Point(68, 118);
+            this.cbTorekeny.Name = "cbTorekeny";
+            this.cbTorekeny.Size = new System.Drawing.Size(71, 17);
+            this.cbTorekeny.TabIndex = 9;
+            this.cbTorekeny.Text = "Törékeny";
+            this.cbTorekeny.UseVisualStyleBackColor = true;
+            this.cbTorekeny.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(231, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(230, 95);
+            this.listBox1.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(231, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Termékek";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(507, 39);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(230, 95);
+            this.listBox2.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(468, 63);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(33, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = ">>";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(507, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Megrendelések";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(551, 159);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Összes érték:";
+            // 
+            // lblOsszertek
+            // 
+            this.lblOsszertek.AutoSize = true;
+            this.lblOsszertek.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblOsszertek.Location = new System.Drawing.Point(629, 159);
+            this.lblOsszertek.Name = "lblOsszertek";
+            this.lblOsszertek.Size = new System.Drawing.Size(37, 13);
+            this.lblOsszertek.TabIndex = 16;
+            this.lblOsszertek.Text = "0,- Ft";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(231, 148);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 23);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Fájlba KIÍR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(361, 148);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 23);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "Fájlból BEOLVAS";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbUresLista);
+            this.groupBox1.Controls.Add(this.rbHozzafuz);
+            this.groupBox1.Location = new System.Drawing.Point(346, 177);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(115, 75);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Beolvasás módja";
+            // 
+            // rbHozzafuz
+            // 
+            this.rbHozzafuz.AutoSize = true;
+            this.rbHozzafuz.Location = new System.Drawing.Point(7, 20);
+            this.rbHozzafuz.Name = "rbHozzafuz";
+            this.rbHozzafuz.Size = new System.Drawing.Size(69, 17);
+            this.rbHozzafuz.TabIndex = 0;
+            this.rbHozzafuz.Text = "Hozzáfűz";
+            this.rbHozzafuz.UseVisualStyleBackColor = true;
+            // 
+            // rbUresLista
+            // 
+            this.rbUresLista.AutoSize = true;
+            this.rbUresLista.Checked = true;
+            this.rbUresLista.Location = new System.Drawing.Point(7, 44);
+            this.rbUresLista.Name = "rbUresLista";
+            this.rbUresLista.Size = new System.Drawing.Size(68, 17);
+            this.rbUresLista.TabIndex = 1;
+            this.rbUresLista.TabStop = true;
+            this.rbUresLista.Text = "Üres lista";
+            this.rbUresLista.UseVisualStyleBackColor = true;
+            // 
+            // btnTorol
+            // 
+            this.btnTorol.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnTorol.ForeColor = System.Drawing.Color.Red;
+            this.btnTorol.Location = new System.Drawing.Point(188, 116);
+            this.btnTorol.Name = "btnTorol";
+            this.btnTorol.Size = new System.Drawing.Size(23, 23);
+            this.btnTorol.TabIndex = 20;
+            this.btnTorol.Text = "X";
+            this.btnTorol.UseVisualStyleBackColor = true;
+            this.btnTorol.Click += new System.EventHandler(this.btnTorol_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTorol);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.lblOsszertek);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.cbTorekeny);
+            this.Controls.Add(this.btnGeneral);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbAr);
+            this.Controls.Add(this.tbDarab);
+            this.Controls.Add(this.tbNev);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Megrendelés";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbNev;
+        private System.Windows.Forms.TextBox tbDarab;
+        private System.Windows.Forms.TextBox tbAr;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnGeneral;
+        private System.Windows.Forms.CheckBox cbTorekeny;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblOsszertek;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbUresLista;
+        private System.Windows.Forms.RadioButton rbHozzafuz;
+        private System.Windows.Forms.Button btnTorol;
+    }
+}
+
---
+++ 2022-03-09-bz/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2022_03_09_bz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class Termek
+        {
+            public string nev;
+            public int db;
+            public int ar;
+            public string torekeny;
+        }
+        List<Termek> lista_termek = new List<Termek>();
+        int osszertek = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGeneral_Click(object sender, EventArgs e)
+        {
+            Random veletlen = new Random();
+            tbDarab.Text = Convert.ToString(veletlen.Next(1, 500));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // ListBox1-be írás
+            string nev = tbNev.Text;
+            string darab = tbDarab.Text;
+            string ar = tbAr.Text;
+            string torekeny;
+            if (cbTorekeny.Checked == true)
+            {
+                torekeny = "Törékeny";
+            }
+            else
+            {
+                torekeny = "NEM törékeny";
+            }
+            string sor = nev + "," + darab + "," + ar + "," + torekeny;
+            listBox1.Items.Add(sor);
+            tbNev.Text = "";
+            tbDarab.Text = "";
+            tbAr.Text = "";
+            cbTorekeny.Checked = false;
+
+            // Termék listába rakás
+            Termek t1 = new Termek();
+            t1.nev = nev;
+            t1.db = Convert.ToInt32(darab);
+            t1.ar = Convert.ToInt32(ar);
+            t1.torekeny = torekeny;
+            lista_termek.Add(t1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // TextBoxba visszaírás ListBox1-ből
+            int kivalasztott = listBox1.SelectedIndex;
+            if (kivalasztott == -1)
+            {
+                MessageBox.Show("Nincs kijelölt elem!");
+            }
+            else
+            {
+                String[] adatsor = new String[3];
+                adatsor = listBox1.SelectedItem.ToString().Split(',');
+                tbNev.Text = adatsor[0];
+                tbDarab.Text = adatsor[1];
+                tbAr.Text = adatsor[2];
+                string torekeny = adatsor[3];
+                if (torekeny == "Törékeny")
+                {
+                    cbTorekeny.Checked = true;
+                }
+                else
+                {
+                    cbTorekeny.Checked = false;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Megrendeléshez hozzáadás
+            int kivalasztott = listBox1.SelectedIndex;
+            if (kivalasztott == -1)
+            {
+                MessageBox.Show("Nincs kiválasztott elem!");
+            }
+            else
+            {
+                string atvitelre = listBox1.SelectedItem.ToString();
+                listBox2.Items.Add(atvitelre);
+                String[] adatsor = new String[3];
+                adatsor = listBox1.SelectedItem.ToString().Split(',');
+                int ertek = Convert.ToInt32(adatsor[2]);
+                osszertek = osszertek + ertek;
+                lblOsszertek.Text = Convert.ToString(osszertek) + ",- Ft";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Fájlba kiírás
+            StreamWriter kiir = new StreamWriter("fajlnev.txt");
+            foreach (Termek t in lista_termek)
+            {
+                kiir.WriteLine(t.nev + "," + t.db + "," + t.ar + "," + t.torekeny);
+            }
+            kiir.Flush();
+            kiir.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Fájlból beolvasás 
+            StreamReader beolvas = new StreamReader("fajlnev.txt");
+            string str_beolvas = beolvas.ReadLine();
+            if (rbUresLista.Checked == true)
+            {
+                listBox1.Items.Clear();
+            }
+            
+            while (str_beolvas != null)
+            {
+                listBox1.Items.Add(str_beolvas);
+                str_beolvas = beolvas.ReadLine();
+            }
+
+            beolvas.Close();
+        }
+
+        private void btnTorol_Click(object sender, EventArgs e)
+        {
+            int kivalasztott = listBox1.SelectedIndex;
+            if (kivalasztott == -1)
+            {
+                MessageBox.Show("Nem törölhető elem!");
+            }
+            else
+            {
+                listBox1.Items.RemoveAt(kivalasztott);
+            }
+        }
+    }
+}
